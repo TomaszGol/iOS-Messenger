f5ee816e5ea421da7803e88caf8c646b04be7113
TomaszGolkowski <tomek@MacBook-Pro-TomaszGolkowski.local>
2021-07-01 23:07:34 +0200

    Commit d

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by TomaszGolkowski on 01/07/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
