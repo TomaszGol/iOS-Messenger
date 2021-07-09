cb5001552b84f331b1f02f955afa3ba02be3c085
TomaszGol <oplane2000@gmail.com>
2021-07-09 14:42:14 +0200

    UI&Dependecies

---
+++ Messenger/Controllers/ChatViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ChatViewController.swift
+//  Messenger
+//
+//  Created by TomaszGolkowski on 09/07/2021.
+//
+
+import UIKit
+import MessageKit
+
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+struct Sender: SenderType {
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "", senderId: "1", displayName: "Thomas")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender, messageId: "1", sentDate: Date(), kind: .text("Hi Elon")))
+        
+        messages.append(Message(sender: selfSender, messageId: "2", sentDate: Date(), kind: .text("What is up? Today I was on bike in california hills, It was awesome. Beautiful views and climate. I love it!!!")))
+        
+        view.backgroundColor = .red
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+    
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+
+}
